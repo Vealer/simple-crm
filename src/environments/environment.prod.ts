4923ffb0758cc5f64b12f4de9d0cbbbd6d16cc2c
Valer Neufeld <valerneufeld@outlook.de>
2022-11-01 13:12:26 +0100

    ##

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-42080',
+    appId: '1:31095944449:web:ef395a74e4281b18d15700',
+    storageBucket: 'simple-crm-42080.appspot.com',
+    apiKey: 'AIzaSyDvx2b9PwqKw_E3OnT8CSvsUlCtMgaJu_o',
+    authDomain: 'simple-crm-42080.firebaseapp.com',
+    messagingSenderId: '31095944449',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-42080',
+    appId: '1:31095944449:web:ef395a74e4281b18d15700',
+    storageBucket: 'simple-crm-42080.appspot.com',
+    apiKey: 'AIzaSyDvx2b9PwqKw_E3OnT8CSvsUlCtMgaJu_o',
+    authDomain: 'simple-crm-42080.firebaseapp.com',
+    messagingSenderId: '31095944449',
+  },
   production: false
 };
 
